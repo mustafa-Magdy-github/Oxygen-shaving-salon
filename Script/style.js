6c1f210efcb6cc1d12e45c9d43a7f78b83927204
mostafamagdy <mustafamagdy020@gmail.com>
2020-09-21 10:29:58 +0200

    first commit hello

---
+++ Script/style.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $(".navbar-toggler-icon").mouseenter(function(){
+        $(".fa-toggle-on").css("color","tomato")
+        
+    });
+
+
+    $(".navbar-toggler-icon").mouseleave(function(){
+        $(".fa-toggle-on").css("color","whitesmoke")
+        
+    });
+
+});
+
+
+
